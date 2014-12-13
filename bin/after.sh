6eeceae362da6eed7583354d10e3fb41ceced697
Joost de Valk <joost@yoast.com>
2014-12-13 12:18:34 +0100

    More changes to Travis setup, let's see if this fixes CodeClimate

--- bin/after.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-CODECLIMATE_REPO_TOKEN="a1ed4996e9da5703ae6483f5a3294cb9c5729dfae8820c453c768f9f31044246"
-./bin/test-reporter --stdout > codeclimate.json
-curl -X POST -d @codeclimate.json -H 'Content-Type: application/json' -H 'User-Agent: Code Climate (PHP Test Reporter v0.1.1)' https://codeclimate.com/test_reports
