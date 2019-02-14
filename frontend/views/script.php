cb8290e09e6557de0ce8388374e60165fd6a17c7
Jimmy Comack <jimmy@yoast.com>
2019-02-14 14:38:11 +0100

    Reverted some of the code to include the extra clicky data

--- frontend/views/script.php
+++ frontend/views/script.php
@@ -3,5 +3,15 @@
  * @package Yoast/Clicky/View
  */
 
-?><script>var clicky_site_ids = clicky_site_ids || []; clicky_site_ids.push(<?php echo $this->options['site_id']; ?>);</script>
+?>
+<script>
+	<?php
+	if ( ! empty( $clicky_extra ) ) {
+		echo 'var clicky_custom = clicky_custom || {}; ';
+		echo $clicky_extra;
+	} ?>
+
+    var clicky_site_ids = clicky_site_ids || [];
+    clicky_site_ids.push(<?php echo $this->options['site_id']; ?>);
+</script>
 <script async src="//static.getclicky.com/js"></script>
