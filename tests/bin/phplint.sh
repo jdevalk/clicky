befef34dc6fcc8c6c843f48c49bd796200ee9c16
jrfnl <jrfnl@users.noreply.github.com>
2019-05-08 18:04:44 +0200

    Travis: don't lint the vendor directory

--- tests/bin/phplint.sh
+++ tests/bin/phplint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-find -L . -name '*.php' -print0 | xargs -0 -n 1 -P 4 php -l
+find -L . -path ./vendor -prune -o -path ./node_modules -prune -o -name '*.php' -print0 | xargs -0 -n 1 -P 4 php -l
