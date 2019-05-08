31680a8cf6e4e152ecad9cd89e34ca4f9689c6b9
jrfnl <jrfnl@users.noreply.github.com>
2019-05-08 18:40:18 +0200

    Travis: fix reporting of code coverage to CodeClimate

--- tests/bin/code_climate.sh
+++ tests/bin/code_climate.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 vendor/bin/phpunit --coverage-clover build/logs/clover.xml
-./bin/test-reporter --stdout > codeclimate.json
+vendor/bin/test-reporter --stdout > codeclimate.json
 curl -X POST -d @codeclimate.json -H "Content-Type\: application/json" -H 'User-Agent: Code Climate (PHP Test Reporter v0.1.1)' https://codeclimate.com/test_reports
