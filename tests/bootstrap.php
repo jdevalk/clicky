d2e2bb65c26062767732f74e04ebbf4f8ca23491
jrfnl <jrfnl@users.noreply.github.com>
2019-05-14 19:31:59 +0200

    Test bootstrap: show friendly error message on failure
    Do more rigorous checking whether the environment is correctly set up for the unit tests to be run and if not, show an instructive error message.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -21,10 +21,30 @@ $GLOBALS['wp_tests_options'] = array(
 );
 
 if ( defined( 'WP_DEVELOP_DIR' ) ) {
-	require WP_DEVELOP_DIR . 'tests/phpunit/includes/bootstrap.php';
+	if ( file_exists( WP_DEVELOP_DIR . 'tests/phpunit/includes/bootstrap.php' ) ) {
+		require WP_DEVELOP_DIR . 'tests/phpunit/includes/bootstrap.php';
+	}
+	else {
+		echo PHP_EOL, 'ERROR: Please check the WP_DEVELOP_DIR environment variable. Based on the current value ', WP_DEVELOP_DIR, ' the WordPress native unit test bootstrap file could not be found.', PHP_EOL;
+		exit( 1 );
+	}
+}
+elseif ( file_exists( '../../../../tests/phpunit/includes/bootstrap.php' ) ) {
+	require '../../../../tests/phpunit/includes/bootstrap.php';
 }
 else {
-	require '../../../../tests/phpunit/includes/bootstrap.php';
+	echo PHP_EOL, 'ERROR: The WordPress native unit test bootstrap file could not be found. Please set the WP_DEVELOP_DIR environment variable either in your OS or in a custom phpunit.xml file.', PHP_EOL;
+	exit( 1 );
+}
+
+if ( file_exists( dirname( dirname( __FILE__ ) ) . '/vendor/autoload_52.php' ) === false ) {
+	echo PHP_EOL, 'ERROR: Run `composer install` to generate the autoload files before running the unit tests.', PHP_EOL;
+	exit( 1 );
+}
+
+if ( ! defined( 'WP_PLUGIN_DIR' ) || file_exists( WP_PLUGIN_DIR . '/clicky/clicky.php' ) === false ) {
+	echo PHP_EOL, 'ERROR: Please check whether the WP_PLUGIN_DIR environment variable is set and set to the correct value. The unit test suite won\'t be able to run without it.', PHP_EOL;
+	exit( 1 );
 }
 
 // Include unit test base class.
