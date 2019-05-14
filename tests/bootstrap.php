a5320c99ebf5949c057676621834424691c60fbb
jrfnl <jrfnl@users.noreply.github.com>
2019-05-14 19:23:54 +0200

    Test bootstrap: remove duplicate function calls

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,6 +8,10 @@ if ( function_exists( 'xdebug_disable' ) ) {
 echo 'Welcome to the Clicky Test Suite' . PHP_EOL;
 echo 'Version: 1.0' . PHP_EOL . PHP_EOL;
 
+if ( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
+	define( 'WP_DEVELOP_DIR', getenv( 'WP_DEVELOP_DIR' ) );
+}
+
 if ( false !== getenv( 'WP_PLUGIN_DIR' ) ) {
 	define( 'WP_PLUGIN_DIR', getenv( 'WP_PLUGIN_DIR' ) );
 }
@@ -16,8 +20,8 @@ $GLOBALS['wp_tests_options'] = array(
 	'active_plugins' => array( 'clicky/clicky.php' ),
 );
 
-if ( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
-	require getenv( 'WP_DEVELOP_DIR' ) . 'tests/phpunit/includes/bootstrap.php';
+if ( defined( 'WP_DEVELOP_DIR' ) ) {
+	require WP_DEVELOP_DIR . 'tests/phpunit/includes/bootstrap.php';
 }
 else {
 	require '../../../../tests/phpunit/includes/bootstrap.php';
