7ec853e32634b6ef01178701c2610c2a7ae43fa9
Jip <jhp.moors@gmail.com>
2020-09-23 18:45:23 +0200

    Exclude functions sniff for tests bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -26,20 +26,38 @@ if ( empty( $_tests_dir ) && getenv( 'WP_DEVELOP_DIR' ) !== false ) {
 // Give access to tests_add_filter() function.
 require_once rtrim( $_tests_dir, '/' ) . '/includes/functions.php';
 
-// Add plugin to active mu-plugins - to make sure it gets loaded.
-tests_add_filter( 'muplugins_loaded', function() {
+/**
+ * Manually load the plugin being tested.
+ */
+function _manually_load_plugin() {
 	require dirname( __DIR__ ) . '/clicky.php';
-} );
+}
 
-// Overwrite the plugin URL to not include the full path.
-tests_add_filter( 'plugins_url', function( $url, $path, $plugin ) {
+/**
+ * Filter the plugins URL to pretend the plugin is installed in the test environment.
+ *
+ * @param string $url    The complete URL to the plugins directory including scheme and path.
+ * @param string $path   Path relative to the URL to the plugins directory. Blank string
+ *                       if no path is specified.
+ * @param string $plugin The plugin file path to be relative to. Blank string if no plugin
+ *                       is specified.
+ *
+ * @return string
+ */
+function _plugins_url( $url, $path, $plugin ) {
 	$plugin_dir = dirname( __DIR__ );
 	if ( $plugin === $plugin_dir . '/clicky.php' ) {
 		$url = str_replace( dirname( $plugin_dir ), '', $url );
 	}
 
 	return $url;
-}, 10, 3 );
+}
+
+// Add plugin to active mu-plugins - to make sure it gets loaded.
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
+// Overwrite the plugin URL to not include the full path.
+tests_add_filter( 'plugins_url', '_plugins_url', 10, 3 );
 
 // Start up the WP testing environment.
 require $_tests_dir . '/includes/bootstrap.php';
