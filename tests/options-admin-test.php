3985999acaa08fb770acd99e561b11f39c28df7a
jrfnl <jrfnl@users.noreply.github.com>
2021-09-28 17:52:51 +0200

    Integration tests: switch to snake_case fixtures
    Switches over `setUpBeforeClass()` to `set_up_before_class()`.
    
    Includes fixing:
    * Missing calls to the `parent::set_up_before_class()`.

--- tests/options-admin-test.php
+++ tests/options-admin-test.php
@@ -30,7 +30,8 @@ class Clicky_Options_Admin_Test extends Clicky_UnitTestCase {
 	/**
 	 * Set up the class instance to be tested.
 	 */
-	public static function setUpBeforeClass() {
+	public static function set_up_before_class() {
+		parent::set_up_before_class();
 		self::$class_instance = new Clicky_Options_Admin();
 	}
 
